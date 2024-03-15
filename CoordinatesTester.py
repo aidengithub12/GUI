c1f971ee366f6636d754a75c18ed58fd1500e9c4
Aiden Brooks <aiden656brooks@gmail.com>
2024-03-15 14:18:51 -0500

    mouse coordinate logger

---
+++ CoordinatesTester.py
@@ -0,0 +1,15 @@
+import pyglet as pg
+import MouseHandler
+resolution = [1920,1080]
+window = pg.window.Window(width = resolution[0], height = resolution[1])
+
+MOUSE = MouseHandler.MouseStateHandler()
+window.push_handlers(MOUSE)
+
+
+@window.event
+def update(dt):
+    print('(' + str(window._mouse_x) + ', ' + str(window._mouse_y) + ')')
+
+pg.clock.schedule_interval(update, 2)
+pg.app.run()
